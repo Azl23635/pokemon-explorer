7f772394ea9ec9a936b415ca19f6f4aa5bd45245
Azl23635 <rmitanzel@gmail.com>
2022-08-16 02:35:43 +1000

    new: dynamic images/tiles

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import { useState } from 'react';
 import './App.css';
 import GeneratePokemon from './GeneratePokemon';
 import ViewPokemon from './ViewPokemon';
+import Logo from './Logo';
 
 function App() {
   const [pokeArr, setPoke] = useState([]);
+  const [isOpaque, setOpacity] = useState('isOpaque');
+  const [imgLoad, setLoaded] = useState(false);
   return (
     <div className="App">
-      <GeneratePokemon pokeArr={pokeArr} setPoke={setPoke} />
-      <ViewPokemon pokeArr={pokeArr} />
+      <Logo />
+      <GeneratePokemon
+        pokeArr={pokeArr}
+        setPoke={setPoke}
+        setOpacity={setOpacity}
+        imgLoad={imgLoad}
+        setLoaded={setLoaded}
+      />
+      <ViewPokemon
+        pokeArr={pokeArr}
+        isOpaque={isOpaque}
+        setLoaded={setLoaded}
+      />
     </div>
   );
 }
