c06f341451b83b4be3383dabaf33fe3a8f521c81
Azl23635 <rmitanzel@gmail.com>
2022-08-16 02:40:12 +1000

    init: Heading section

---
+++ src/Heading.jsx
@@ -0,0 +1,26 @@
+const Heading = () => {
+  return (
+    <div className="logoContainer">
+      <div className="headings">
+        <img
+          className="pokeLogo"
+          src="src/assets/pokemonLogo.png"
+          alt="pokemon logo"
+        />
+        <h1>Random Generator</h1>
+        <div className="container">
+          <div className="wrapper">
+            <h3>
+              Do <em>you</em> know all the Pokemon?
+            </h3>
+            <p>
+              Generate random pokemon to increase your knowledge. See
+              if you can find <b>Rayquaza</b>!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Heading;
