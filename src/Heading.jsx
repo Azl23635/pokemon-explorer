2c5882cb30b6f698cff9f5b83ed3930c4566c156
Azl23635 <rmitanzel@gmail.com>
2022-08-16 05:25:25 +1000

    deploy

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -4,7 +4,7 @@ const Heading = () => {
       <div className="headings">
         <img
           className="pokeLogo"
-          src="/public/pokemonLogo.png"
+          src="pokemonLogo.png"
           alt="pokemon logo"
         />
         <h1>Random Generator</h1>
